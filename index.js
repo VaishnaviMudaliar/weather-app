3e6c68778344601726ac42fc70f3d4bf1f96b504
Vaishnavi Mudaliar <vaishnavim1311@gmail.com>
2021-11-08 10:24:52 +0000

    api integration and final touches

--- index.js
+++ index.js
@@ -4,13 +4,35 @@ const request = require('request');
 
 const app = express()
 
-const apiKey = ''
+const apiKey = '0526b523e212e6d1b780cb2edd273c55';
 
 app.use(express.static('public'))
 
 app.use(bodyparser.urlencoded({extended:true}))
 
 app.set('view engine','ejs')
+
+app.post('/',function (req,res){
+  let city = req.body.city
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  console.log(req.body.city)
+  request(url,function(err,response,body){
+    if(err){
+      res.render('index',{weather:null,error : 'Error please try again'})
+    }
+    else{
+          let weather = JSON.parse(body)
+          if(weather.main == undefined){
+            res.render('index', {weather : null , error : 'Error please try again'})
+          }
+          else{
+            let weatherText = `Its ${weather.main.temp} degrees Celcius with ${weather.weather[0].main} in ${weather.name}!`
+            res.render('index',{weather:weatherText,error:null})
+            console.log('body:',body);
+          }
+    }
+  })
+})
 
 app.get('/',function(req,res){
   res.render('index',{weather : null, error : null})
